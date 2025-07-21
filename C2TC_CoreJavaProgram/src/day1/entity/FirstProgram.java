f1bbd7f3fd817cc164e35b71ad29cd3cb7dcf9ce
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-21 20:49:53 +0530

    day one assignments

---
+++ C2TC_CoreJavaProgram/src/day1/entity/FirstProgram.java
@@ -0,0 +1,11 @@
+package day1.entity;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+
+	}
+
+}
---
+++ C2TC_CoreJavaProgram/src/day1/entity/IdentifersDemo.java
@@ -0,0 +1,12 @@
+package day1.entity;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		int $number = 20;
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Aniket";
+		System.out.println("value is : "+ studentName);
+		}
+	}
+
