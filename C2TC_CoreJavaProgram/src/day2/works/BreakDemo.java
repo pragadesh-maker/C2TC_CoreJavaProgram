fcd95cba25847459800f9050b5ef0b291aa207b4
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-22 09:32:14 +0530

    day two assignments

---
+++ C2TC_CoreJavaProgram/src/day2/works/BreakDemo.java
@@ -0,0 +1,15 @@
+package day2.works;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		for (int i = 5; i < 10; i++) {
+			if (i == 10)
+				break;
+			System.out.println(i);
+		}
+
+
+	}
+
+}
---
+++ C2TC_CoreJavaProgram/src/day2/works/WhileDemo.java
@@ -0,0 +1,15 @@
+package day2.works;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
