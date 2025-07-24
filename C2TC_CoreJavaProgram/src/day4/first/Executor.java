0203d08c6836a1110892e0444ba4d8f51f665ffe
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-24 13:49:12 +0530

    day-4 first work

---
+++ C2TC_CoreJavaProgram/src/day4/first/Executor.java
@@ -0,0 +1,24 @@
+package day4.first;
+
+public class Executor {
+	public static void main(String[] args) {
+		Base b1=new Base();
+	
+	b1.methodDefault();
+	b1.methodProtected();
+	b1.methodPublic();
+	
+	b1.varDefault=11;
+	b1.methodDefault();
+	
+	b1.varProtected=31;
+	b1.methodProtected();
+	
+	b1.varPublic=41;
+	b1.methodPublic();
+	
+	//instance of operator
+	System.out.println(b1 instanceof Base);
+
+}
+}
