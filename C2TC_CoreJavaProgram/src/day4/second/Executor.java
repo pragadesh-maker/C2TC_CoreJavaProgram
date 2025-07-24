6ffb71fb27c64ab7811350efb616876c0defd6a8
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-24 13:58:51 +0530

    day-4 second work

---
+++ C2TC_CoreJavaProgram/src/day4/second/Executor.java
@@ -0,0 +1,14 @@
+package day4.second;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		Base b1 = new Base();
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+
+
+	}
+
+}
