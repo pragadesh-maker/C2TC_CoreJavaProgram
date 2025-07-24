f02ed963a39514239e20b98a5c87c823881db421
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-24 09:50:37 +0530

    day 3 works

---
+++ C2TC_CoreJavaProgram/src/day1/basic/UserName.java
@@ -0,0 +1,13 @@
+package day1.basic;
+import java.util.Scanner;
+public class UserName {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+				System.out.println("enter your name; ");
+				String n=input.next();
+				System.out.println("hello " + n);
+
+	}
+
+}
