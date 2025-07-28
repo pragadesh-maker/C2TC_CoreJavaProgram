dc66b3a3f01ad8608fd683c9c8d19023bf388364
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-28 15:32:36 +0530

    day6-Static method

---
+++ C2TC_CoreJavaProgram/src/day6/staticmethodblock/Myclass.java
@@ -0,0 +1,26 @@
+package day6.staticmethodblock;
+
+public class Myclass {
+	private int section ; 
+	private static int srNo;
+	static
+	{
+		System.out.println("-----------Within Static Block---------");
+		srNo=1000;
+	}
+	MyClass()
+	{
+		System.out.println("--------------Within Default Constructor-----------");
+		srNo++;
+		section++;		
+	}
+	public String toString() {
+		return "MyClass [Serial No "+srNo+", Section=" + section + "]";
+	}
+	static void display() 
+	{
+		System.out.println("Serial No. "+srNo); 
+	}
+
+
+}
