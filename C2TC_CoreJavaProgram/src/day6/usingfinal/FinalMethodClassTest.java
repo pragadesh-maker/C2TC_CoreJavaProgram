ddcf040b26e68ef6bf51571941694d24a8fae620
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-28 15:40:17 +0530

    day6-using final

---
+++ C2TC_CoreJavaProgram/src/day6/usingfinal/FinalMethodClassTest.java
@@ -0,0 +1,6 @@
+package day6.usingfinal;
+
+public class FinalMethodClassTest extends FinalMethodClass  {
+	void show() {
+		System.out.println("This is the final method of FinalMethodEx class"); }
+	}
