8e813fabfbe096ab50bc315cb5c270f440f15b6c
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-28 15:36:25 +0530

    day6-static variable

---
+++ C2TC_CoreJavaProgram/src/day6/staticvariable/Employee.java
@@ -0,0 +1,16 @@
+package day6.staticvariable;
+
+public class Employee {
+		private String name;
+		private int id;
+		static String companyName = "TNS";
+		Employee(String name, int id) {
+			this.name = name;
+			this.id = id;
+		}
+		public String toString() {
+			return "Employee [name=" + name + ", id=" + id + ", Company= "+companyName+"]";
+		}
+
+
+}
