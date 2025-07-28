577631ddf9628a5cd8b85389ad10d832e67229f8
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-28 09:22:40 +0530

    day5-multilevel inheriance

---
+++ C2TC_CoreJavaProgram/src/day5/multilevel/vehicle/Car.java
@@ -0,0 +1,14 @@
+package day5.multilevel.vehicle;
+
+public class Car {
+	public Car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType() {
+		System.out.println("Vehicle Type: Car");
+	}
+
+
+
+}
