66f9846b2f0207cb9745557cb5c82e799a1ead2d
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-29 09:14:30 +0530

    day 7 practice

---
+++ C2TC_CoreJavaProgram/src/Abstractexample/Trainer.java
@@ -0,0 +1,14 @@
+package Abstractexample;
+public class Trainer extends Coder {
+	public void rules()
+	{
+		System.out.println("Morning wake up at "+wakeup);
+		System.out.println("and check student upload ");
+	}
+
+
+
+}
+
+
+
