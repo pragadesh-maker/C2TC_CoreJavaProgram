b3df6d5e0cbb51faa09a16201f095c6f834c4f2e
Acer <Acer@DESKTOP-QDHA5L2>
2025-07-31 13:46:04 +0530

    day 11 practice

---
+++ C2TC_CoreJavaProgram/src/day11/v1/FinallyBlock.java
@@ -0,0 +1,22 @@
+package day11.v1;
+
+public class FinallyBlock {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		try {
+			System.out.print("Enter the number : ");
+			int number = scanner.nextInt();
+			System.out.println(number+number % 2 == 0 ? " is Even" : " is Odd");
+			return;
+		} catch (InputMismatchException e) {
+			System.err.println("Invalid input...");
+
+		} finally {
+			System.out.println("In Finally....");
+			scanner.close(); 
+		}
+
+	}
+
+}
